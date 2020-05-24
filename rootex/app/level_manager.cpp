a429eaada2af8ec09f2379eeec20469464688d8f
capricious_rebel <codingfreak98@gmail.com>
2020-05-24 06:55:24 -0700

    seperate setter and update functions for listener

--- rootex/app/level_manager.cpp
+++ rootex/app/level_manager.cpp
@@ -50,17 +50,14 @@ void LevelManager::openLevel(const String& levelPath, bool openInEditor)
 		RenderSystem::GetSingleton()->setCamera(cameraEntity->getComponent<CameraComponent>().get());
 	}
 
-	if (m_CurrentLevelSettings.find("listener") != m_CurrentLevelSettings.end())
-	{
-	}
-	else
+	if (!(m_CurrentLevelSettings.find("listener") != m_CurrentLevelSettings.end()))
 	{
 		m_CurrentLevelSettings["listener"] = ROOT_ENTITY_ID;
 		saveCurrentLevelSettings();
 	}
 	{
 		Ref<Entity> listenerEntity = EntityFactory::GetSingleton()->findEntity(m_CurrentLevelSettings["listener"]);
-		AudioSystem::GetSingleton()->setListener(listenerEntity->getComponent<TransformComponent>().get());
+		AudioSystem::GetSingleton()->setListenerComponent(listenerEntity->getComponent<TransformComponent>().get());
 	}
 
 	if (!openInEditor)
@@ -98,7 +95,6 @@ void LevelManager::createLevel(const String& newLevelName)
 	newLevelJSON["camera"] = ROOT_ENTITY_ID;
 	newLevelJSON["inputSchemes"] = JSON::json::array();
 	newLevelJSON["startScheme"] = "";
-	// attach listener to ROOT ENTITY
 	newLevelJSON["listener"] = ROOT_ENTITY_ID;
 	OS::CreateFileName("game/assets/levels/" + newLevelName + "/" + newLevelName + ".level.json") << newLevelJSON.dump(1, '\t');
 
